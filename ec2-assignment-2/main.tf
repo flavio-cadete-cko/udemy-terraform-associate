f9f0123fa98055a85dd6ca9711cd02fda32e9c94
flavio-cadete-cko <flavio.cadete@checkout.com>
2021-03-08 14:59:00 +0100

    EC2-assignment-2

--- ec2-assignment-2/main.tf
+++ ec2-assignment-2/main.tf
@@ -1,5 +1,15 @@
 #Create your provider configuration
-
+provider "aws" {
+    region = "eu-west-1"
+}
 
 #Create an EC2 Instance with Tags
-
+resource "aws_instance" "flc_ec2_assignment_2" {
+    ami = "ami-0debf1d5c6ecf21b8"
+    instance_type = "t2.micro"
+    #Add Tags
+    tags = {
+        Name = "flc_ec2_assignment_2"
+        environment = "playground"
+    }
+}
--- ec2-lab-3/main.tf
+++ ec2-lab-3/main.tf
@@ -5,8 +5,11 @@ provider "aws" {
 
 #EC2 Block w/tags
 resource "aws_instance" "tags-test" {
-    ami = "ami-0947d2ba12ee1ff75"
+    ami = "ami-0debf1d5c6ecf21b8"
     instance_type = "t2.micro"
     #Add Tags
-
+    tags = {
+        Name = "flc_ec2_lab_3"
+        environment = "playground"
+    }
 }

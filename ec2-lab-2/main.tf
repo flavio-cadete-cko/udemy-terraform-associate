3a74be5a807748cf3bb95a9dfa1fee5e178377ae
flavio-cadete-cko <flavio.cadete@checkout.com>
2021-03-08 14:37:57 +0100

    EC2-lab-2

--- ec2-lab-2/main.tf
+++ ec2-lab-2/main.tf
@@ -3,3 +3,11 @@ provider "aws" {
 }
 
 #Create an EC2 Resource Block
+resource "aws_instance" "flc_ec2_lab_2" {
+    ami = "ami-0aa2b94859c32a40f"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "flc_ec2_lab_2"
+    }
+}

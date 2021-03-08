38450ffb7f2981d148f91361231987d4a610f977
flavio-cadete-cko <flavio.cadete@checkout.com>
2021-03-08 15:33:22 +0100

    Variables

--- ec2-lab-4/main.tf
+++ ec2-lab-4/main.tf
@@ -1,19 +1,24 @@
 provider "aws" {
-    region = "YOUR REGION"
+    region = "eu-west-1"
 }
 
-resource "aws_instance" "tags-test" {
-    ami = "YOUR AMI"
+resource "aws_instance" "flc-tags-test" {
+    ami = "ami-0debf1d5c6ecf21b8"
     instance_type = "t2.micro"
+    #Add Tags
+    tags = {
+        Name = "flc_ec2_lab_3"
+        environment = "playground"
+    }
 }
 
-# resource "aws_ebs_volume" "example" {
-#   availability_zone = "YOUR AZ"
-#   size              = 40
-# }
+resource "aws_ebs_volume" "flc-example" {
+  availability_zone = "eu-west-1c"
+  size              = 40
+}
 
-# resource "aws_volume_attachment" "ebs_att" {
-#   device_name = "/dev/sdh"
-#   volume_id   = 
-#   instance_id = 
-# }
+resource "aws_volume_attachment" "flc-ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   =  aws_ebs_volume.flc-example.id
+  instance_id = aws_instance.flc-tags-test.id
+}
--- variables-lab-1/main.tf
+++ variables-lab-1/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "YOUR REGION"
+  region = "eu-west-1"
 }
 
 resource "aws_instance" "test_instance" {
   ami           = "YOUR AMI"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
       Name = "test_instance_1"
       environment = "test"
@@ -13,7 +13,7 @@ resource "aws_instance" "test_instance" {
 
 resource "aws_instance" "test_instance_2" {
   ami           = "YOUR AMI"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
       Name = "test_instance_2"
       environment = "test"

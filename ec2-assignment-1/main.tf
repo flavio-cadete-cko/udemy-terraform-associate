be3f4b4323bccbb7a8cba0896c75a2990d91ef1c
flavio-cadete-cko <flavio.cadete@checkout.com>
2021-03-08 14:38:15 +0100

    EC2-Assignment-1

--- ec2-assignment-1/main.tf
+++ ec2-assignment-1/main.tf
@@ -1,5 +1,14 @@
 #Create the aws provide code. Find help at - https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-provider
+provider "aws" {
+    region = "eu-west-1"
+}
 
 #Create the EC2 instance resource code. Find help at - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
-resource
+resource "aws_instance" "flc_ec2_assignment_1" {
+    ami = "ami-0debf1d5c6ecf21b8"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "flc_ec2_assignment_1"
+    }
+}

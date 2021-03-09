b091c8d5395219fe996b3edcbeb4f2456e739837
flavio-cadete-cko <flavio.cadete@checkout.com>
2021-03-09 14:48:47 +0100

    State Commands

--- terraform-state-commands/main.tf
+++ terraform-state-commands/main.tf
@@ -2,18 +2,18 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "test_instance" {
-  ami           = "ami-0947d2ba12ee1ff75"
+resource "aws_instance" "flc_test_instance" {
+  ami           = "ami-0debf1d5c6ecf21b8"
   instance_type = "t2.micro"
 }
 
-resource "aws_instance" "test_instance_2" {
-  ami           = "ami-0947d2ba12ee1ff75"
+resource "aws_instance" "flc_test_instance_2" {
+  ami           = "ami-0debf1d5c6ecf21b8"
   instance_type = "t2.micro"
 }
 
 # resource "aws_instance" "move_to_me" {
-#   ami           = "ami-0947d2ba12ee1ff75"
+#   ami           = "ami-0debf1d5c6ecf21b8"
 #   instance_type = "t2.micro"
 #   tags = {
 #     Name = "moved"

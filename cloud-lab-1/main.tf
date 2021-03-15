a5b601a70c90d25d3b0c84ba671e353ed574d31d
flavio-cadete-cko <flavio.cadete@checkout.com>
2021-03-15 17:28:06 +0100

    missing save

--- cloud-lab-1/main.tf
+++ cloud-lab-1/main.tf
@@ -1,5 +1,22 @@
 #Remote Backend Configuration
+terraform {
+  backend "remote" {
+    organization = "tf-udemy-course"
+
+    workspaces {
+      name = "cli-udemy-terraform-course"
+    }
+  }
+}
 
 #Provider Configuration
+provider "aws" {
+  region     = "eu-west-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
 
 #S3 Bucket Configuration
+resource "aws_s3_bucket" "flc_s3_bucket" {
+  bucket_prefix = "flc-test"
+}
